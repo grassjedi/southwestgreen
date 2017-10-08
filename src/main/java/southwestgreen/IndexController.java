fc183f48462280ac10ba8acad9693cbbf51988ed
Juan Meyer <juan.meyer@brandseye.com>
2017-10-08 19:56:20 +0200

    adding spring security

--- src/main/java/southwestgreen/IndexController.java
+++ src/main/java/southwestgreen/IndexController.java
@@ -2,15 +2,31 @@ package southwestgreen;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Map;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Map<String, Object> model) {
         model.put("message", "damnit");
         return "index";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(){
+        return "logout";
+    }
+
+    @RequestMapping(value = "/denied", method = RequestMethod.GET)
+    public String denied(){
+        return "denied";
     }
 }
